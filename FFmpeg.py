48079b2eb73e77fd0c21b094af794ae0ed5a4970
Parham Monfared <pmmd2000@gmail.com>
2023-12-26 18:32:26 +0000

    First scripts

---
+++ FFmpeg.py
@@ -0,0 +1,22 @@
+import ffmpeg
+
+input_file = '/home/parham/videos/input.mp4'
+output_file = '/home/parham/videos/converted/output.m3u8'
+
+#FFmpeg conversion
+(
+ffmpeg
+.input(input_file)
+.output
+    (
+    output_file, 
+    s='854x480', 
+    vcodec='libx264', 
+    max_muxing_queue_size=9999, 
+    preset='veryfast', 
+    start_number=0, 
+    hls_time=4, 
+    hls_list_size=0
+    )
+.run_async(pipe_stdout=False)
+)
